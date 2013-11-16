9f8f02346216c155276cf8f88e2e2579a5f9458c
BrianMBrannon <brianbrannon@utexas.edu>
2013-11-16 10:18:36 -0600

    Added database - billionth try.

--- ChurningTracker/src/ut/hacktx/churningtracker/Contact.java
+++ ChurningTracker/src/ut/hacktx/churningtracker/Contact.java
@@ -6,22 +6,38 @@ public class Contact {
     int _id;
     String _name;
     String _moneySpent;
+    String _needToSpend;
+    String _bonus;
+    String _fee;
+    String _timeFrame;
+    String _month;
+    
      
     // Empty constructor
     public Contact(){
          
     }
     // constructor
-    public Contact(int id, String name, String _moneySpent){
+    public Contact(int id, String name, String moneySpent, String needToSpend, String bonus, String fee, String timeFrame, String month){
         this._id = id;
         this._name = name;
-        this._moneySpent = _moneySpent;
+        this._moneySpent = moneySpent;
+        this._needToSpend = needToSpend;
+        this._bonus = bonus;
+        this._fee = fee;
+        this._timeFrame = timeFrame;
+        this._month = month;
     }
      
     // constructor
-    public Contact(String name, String _moneySpent){
+    public Contact(String name, String moneySpent, String needToSpend, String bonus, String fee, String timeFrame, String month){
         this._name = name;
-        this._moneySpent = _moneySpent;
+        this._moneySpent = moneySpent;
+        this._needToSpend = needToSpend;
+        this._bonus = bonus;
+        this._fee = fee;
+        this._timeFrame = timeFrame;
+        this._month = month;
     }
     // getting ID
     public int getID(){
@@ -44,12 +60,58 @@ public class Contact {
     }
      
     // getting phone number
-    public String getPhoneNumber(){
+    public String getMoneySpent(){
         return this._moneySpent;
     }
      
     // setting phone number
-    public void setPhoneNumber(String moneySpent){
+    public void setMoneySpent(String moneySpent){
         this._moneySpent = moneySpent;
+    }
+    
+    // getting name
+    public String getNeedToSpend(){
+        return this._needToSpend;
+    }
+     
+    // setting name
+    public void setNeedToSpend(String needToSpend){
+        this._needToSpend = needToSpend;
+    }
+    // getting name
+    public String getBonus(){
+        return this._bonus;
+    }
+     
+    // setting name
+    public void setBonus(String bonus){
+        this._bonus = bonus;
+    }
+    // getting name
+    public String getFee(){
+        return this._fee;
+    }
+     
+    // setting name
+    public void setFee(String fee){
+        this._fee = fee;
+    }
+    // getting name
+    public String getTimeFrame(){
+        return this._timeFrame;
+    }
+     
+    // setting name
+    public void setTimeFrame(String timeFrame){
+        this._timeFrame = timeFrame;
+    }
+    // getting name
+    public String getMonth(){
+        return this._month;
+    }
+     
+    // setting name
+    public void setMonth(String month){
+        this._month = month;
     }
 }
--- ChurningTracker/src/ut/hacktx/churningtracker/DatabaseHandler.java
+++ ChurningTracker/src/ut/hacktx/churningtracker/DatabaseHandler.java
@@ -24,8 +24,13 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     // Contacts Table Columns names
     private static final String KEY_ID = "id";
     private static final String KEY_NAME = "name";
-    private static final String KEY_MONEY_SPENT = "money_spent";
- 
+    private static final String KEY_MONEY_SPENT = "moneySpent";
+    private static final String KEY_NEED_TO_SPEND = "needToSpend";
+    private static final String KEY_BONUS = "bonus";
+    private static final String KEY_FEE = "fee";
+    private static final String KEY_TIME_FRAME = "timeFrame";
+    private static final String KEY_MONTH = "month";
+    
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -59,15 +64,20 @@ public class DatabaseHandler extends SQLiteOpenHelper {
  
         ContentValues values = new ContentValues();
         values.put(KEY_NAME, contact.getName()); // Contact Name
-        values.put(KEY_MONEY_SPENT, contact.getPhoneNumber()); // Contact Phone
- 
+        values.put(KEY_MONEY_SPENT, contact.getMoneySpent()); // Contact Phone
+        values.put(KEY_NEED_TO_SPEND, contact.getNeedToSpend()); // Contact Phone
+        values.put(KEY_BONUS, contact.getBonus()); // Contact Phone
+        values.put(KEY_FEE, contact.getFee()); // Contact Phone
+        values.put(KEY_TIME_FRAME, contact.getTimeFrame()); // Contact Phone
+        values.put(KEY_MONTH, contact.getMonth()); // Contact Phone
+        
         // Inserting Row
         db.insert(TABLE_CONTACTS, null, values);
         db.close(); // Closing database connection
     }
  
-    // Getting single contact
-    Contact getContact(int id) {
+    // Getting single card
+    Contact getCard(int id) {
         SQLiteDatabase db = this.getReadableDatabase();
  
         Cursor cursor = db.query(TABLE_CONTACTS, new String[] { KEY_ID,
@@ -76,15 +86,16 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         if (cursor != null)
             cursor.moveToFirst();
  
-        Contact contact = new Contact(Integer.parseInt(cursor.getString(0)),
-                cursor.getString(1), cursor.getString(2));
+        Contact card = new Contact(Integer.parseInt(cursor.getString(0)),
+                cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4), cursor.getString(5),
+                cursor.getString(6), cursor.getString(7));
         // return contact
-        return contact;
+        return card;
     }
      
-    // Getting All Contacts
-    public List<Contact> getAllContacts() {
-        List<Contact> contactList = new ArrayList<Contact>();
+    // Getting All Cards
+    public List<Contact> getAllCards() {
+        List<Contact> cardList = new ArrayList<Contact>();
         // Select All Query
         String selectQuery = "SELECT  * FROM " + TABLE_CONTACTS;
  
@@ -94,43 +105,54 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         // looping through all rows and adding to list
         if (cursor.moveToFirst()) {
             do {
-                Contact contact = new Contact();
-                contact.setID(Integer.parseInt(cursor.getString(0)));
-                contact.setName(cursor.getString(1));
-                contact.setPhoneNumber(cursor.getString(2));
+                Contact card = new Contact();
+                card.setID(Integer.parseInt(cursor.getString(0)));
+                card.setName(cursor.getString(1));
+                card.setMoneySpent(cursor.getString(2));
+                card.setNeedToSpend(cursor.getString(3));
+                card.setBonus(cursor.getString(4));
+                card.setFee(cursor.getString(5));
+                card.setTimeFrame(cursor.getString(6));
+                card.setMonth(cursor.getString(7));
                 // Adding contact to list
-                contactList.add(contact);
+                cardList.add(card);
             } while (cursor.moveToNext());
         }
  
         // return contact list
-        return contactList;
+        return cardList;
     }
  
     // Updating single contact
-    public int updateContact(Contact contact) {
+    public int updateContact(Contact card) {
         SQLiteDatabase db = this.getWritableDatabase();
  
         ContentValues values = new ContentValues();
-        values.put(KEY_NAME, contact.getName());
-        values.put(KEY_MONEY_SPENT, contact.getPhoneNumber());
+        values.put(KEY_NAME, card.getName());
+        values.put(KEY_MONEY_SPENT, card.getMoneySpent());
+        values.put(KEY_NEED_TO_SPEND, card.getNeedToSpend());
+        values.put(KEY_BONUS, card.getBonus());
+        values.put(KEY_FEE, card.getFee());
+        values.put(KEY_TIME_FRAME, card.getTimeFrame());
+        values.put(KEY_MONTH, card.getMonth());
+        
  
         // updating row
         return db.update(TABLE_CONTACTS, values, KEY_ID + " = ?",
-                new String[] { String.valueOf(contact.getID()) });
+                new String[] { String.valueOf(card.getID()) });
     }
  
-    // Deleting single contact
-    public void deleteContact(Contact contact) {
+    // Deleting single card
+    public void deleteCard(Contact card) {
         SQLiteDatabase db = this.getWritableDatabase();
         db.delete(TABLE_CONTACTS, KEY_ID + " = ?",
-                new String[] { String.valueOf(contact.getID()) });
+                new String[] { String.valueOf(card.getID()) });
         db.close();
     }
  
  
     // Getting contacts Count
-    public int getContactsCount() {
+    public int getCardCount() {
         String countQuery = "SELECT  * FROM " + TABLE_CONTACTS;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(countQuery, null);
